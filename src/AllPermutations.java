5ce0ac9123525b5d433ad68db5e211f2d577a166
Not4Zy <519419490@qq.com>
2017-09-05 16:24:12 -0400

    IDE Commit Test

--- src/AllPermutations.java
+++ src/AllPermutations.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,6 +11,28 @@ public class AllPermutations {
     }
 
     public List<String> permutations(String set) {
-        return null;
+        List<String> result = new ArrayList<>();
+        if (set == null) {
+            return result;
+        }
+        char[] array = set.toCharArray();
+        helper(array, 0, result);
+        return result;
+    }
+
+    /**
+     *
+     * @param array char array by separating input string
+     * @param index as below
+     * @param result result permutations list
+     *
+     * choose the character to be at the position of "index",
+     * all the already chosen positions are (0, index - 1)
+     * all the candidate characters can be at position "index"
+     * are in the sub-array of (index, array.length - 1)
+     *
+     */
+    private void helper(char[] array, int index, List<String> result) {
+
     }
 }
