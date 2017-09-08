e8f39dbf8391a4dc6e541267e819b4f4a4e134c4
Not4Zy <519419490@qq.com>
2017-09-08 04:24:23 -0400

    final

---
+++ src/Smallest_k.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+/**
+ * project: Test
+ *
+ * @author YubaiTao on 08/09/2017.
+ */
+public class Smallest_k {
+    public Smallest_k() {
+
+    }
+
+    private List<Integer> useMinHeap() {
+        return null;
+    }
+
+    private List<Integer> useMaxHeap() {
+        return null;
+    }
+}
