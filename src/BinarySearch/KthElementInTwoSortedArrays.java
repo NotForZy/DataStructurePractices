1b2115222bacbbca5cc9ff61b2813415fd76c5bb
Not4Zy <519419490@qq.com>
2017-12-29 02:42:38 -0500

    Update

--- src/BinarySearch/KthElementInTwoSortedArrays.java
+++ src/BinarySearch/KthElementInTwoSortedArrays.java
@@ -6,6 +6,11 @@ package BinarySearch;
  * @author YubaiTao on 23/12/2017.
  *
  *
+ * Given two sorted arrays of size m and n respectively,
+ * you are tasked with finding the element that would be at the
+ * k-th position of the final sorted array.
+ *
+ *
  * Original Link:
  *     https://www.geeksforgeeks.org/k-th-element-two-sorted-arrays/
  *
@@ -17,7 +22,7 @@ public class KthElementInTwoSortedArrays {
     }
 
     // The time complexity should be constrained in O(logm + logn)
-    public int findKth(int[] array1, int[] array2,
+    private int findKth(int[] array1, int[] array2,
                        int start1, int start2,
                        int end1, int end2, int k) {
         if (start1 == end1) {
@@ -52,13 +57,74 @@ public class KthElementInTwoSortedArrays {
 
     }
 
+    // Divide And Conquer Approach 2
+    // Time complexity O(logk)
+    private int findKthEnhanced(int[] array1, int[] array2, int m, int n, int k, int st1, int st2) {
+        // in case we have reached end of array 1
+        if (st1 == m) {
+            return array2[st2 + k - 1];
+        }
+
+        // in case we have reached end of array 2
+        if (st2 == n) {
+            return array1[st1 + k - 1];
+        }
+
+        // k should never reach 0 or exceed sizes of arrays
+        if (k == 0 || k > (m - st1) + (n - st2)) {
+            return -1;
+        }
+
+        // Compare first elements of arrays and return
+        if (k == 1) {
+            return (array1[st1] < array2[st2]) ? array1[st1] : array2[st2];
+        }
+        int curr = k / 2;
+
+        // size of array 1 is less than k / 2
+        if (curr - 1 >= m - st1) {
+            // Last of element of array1 is not k-th
+            // We can directly return the (k - m)th
+            // element in array 2
+            if (array1[m - 1] < array2[st2 + curr - 1]) {
+                return array2[st2 + (k - (m - st1) - 1)];
+            } else {
+                return findKthEnhanced(array1, array2, m, n, k - curr, st1, st2 + curr);
+            }
+        }
+
+        // size of array 2 is less than k / 2
+        if (curr - 1 >= n - st2) {
+            if (array2[n - 1] < array1[st1 + curr - 1]) {
+                return array1[st1 + (k - (n - st2) - 1)];
+            } else {
+                return findKthEnhanced(array1, array2, m, n, k - curr, st1 + curr, st2);
+            }
+        } else {
+            // Normal comparison, move starting index of one array k / 2 to the right
+            if (array1[curr + st1 - 1] < array2[curr + st2 - 1]) {
+                return findKthEnhanced(array1, array2, m, n, k - curr, st1 + curr, st2);
+            } else {
+                return findKthEnhanced(array1, array2, m, n, k - curr, st1, st2 + curr);
+            }
+        }
+
+    }
+
+
     private void test(){
         int[] array1 = {2, 3, 6, 7, 9};
         int[] array2 = {1, 4, 8, 10};
-        int k = 8;
+        int k = 5;
 
         int result = findKth(array1, array2, 0, 0, array1.length, array2.length, k - 1);
         System.out.print("The " + k + "-th element of two arrays union is: " );
         System.out.println("\t" + result);
+
+        System.out.println("--------------------------------------");
+
+        int result2 = findKthEnhanced(array1, array2, array1.length, array2.length, k, 0, 0);
+        System.out.print("The " + k + "-th element of two arrays union is: " );
+        System.out.println("\t" + result2);
     }
 }
